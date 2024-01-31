8032c0565c3debd9093b5ae8426c7c5e0c27e2df
lakhanAhit <lakhan.singh.rathore.ahit@gmail.com>
2024-01-31 14:44:48 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+
+// <------------------------- FAG Toggle ---------------------------->
+
 const toggles = document.querySelectorAll(".faq-toggle");
 
 toggles.forEach((toggle) => {
@@ -5,3 +8,161 @@ toggles.forEach((toggle) => {
     toggle.parentElement.classList.toggle("active");
   });
 });
+
+// <------------------------- FAG Toggle ---------------------------->
+
+
+// <------------------------- Gallery Section ---------------------------->
+
+function showImage(imageId) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("modalImage");
+
+  modalImg.src = "assets/images/" + imageId;
+  modal.style.display = "block";
+
+  // Close the modal when clicking outside the image
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      closeModal();
+    }
+  };
+}
+
+function closeModal() {
+  var modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+// <------------------------- Gallery Section ---------------------------->
+
+
+// <------------------------- Carousel ---------------------------->
+
+$(document).ready(function(){
+  $(".SlickCarousel").slick({
+    rtl:false, // If RTL Make it true & .slick-slide{float:right;}
+    autoplay:true, 
+    autoplaySpeed:5000, //  Slide Delay
+    speed:800, // Transition Speed
+    slidesToShow:4, // Number Of Carousel
+    slidesToScroll:1, // Slide To Move 
+    pauseOnHover:false,
+    appendArrows:$(".main-contain .Head .Arrows"), // Class For Arrows Buttons
+    prevArrow:'<span class="Slick-Prev"></span>',
+    nextArrow:'<span class="Slick-Next"></span>',
+    easing:"linear",
+    responsive:[
+      {breakpoint:801,settings:{
+        slidesToShow:4,
+      }},
+      {breakpoint:641,settings:{
+        slidesToShow:3,
+      }},
+      {breakpoint:481,settings:{
+        slidesToShow:2,
+      }},
+    ],
+  })
+  })
+
+// <------------------------- Carousel Section ---------------------------->
+
+
+// <------------------------- Header ---------------------------->
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+hamburger.classList.remove("active");
+navMenu.classList.remove("active");
+}
+
+// <------------------------- Header ---------------------------->
+
+
+// <------------------------- Card ---------------------------->
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  
+  breakpoints:{
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+// <------------------------- Card ---------------------------->
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This example creates a simple polygon representing the Bermuda Triangle.
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5,
+    center: { lat: 24.886, lng: -70.268 },
+    mapTypeId: "terrain",
+  });
+  // Define the LatLng coordinates for the polygon's path.
+  const triangleCoords = [
+    { lat: 25.774, lng: -80.19 },
+    { lat: 18.466, lng: -66.118 },
+    { lat: 32.321, lng: -64.757 },
+    { lat: 25.774, lng: -80.19 },
+  ];
+  // Construct the polygon.
+  const bermudaTriangle = new google.maps.Polygon({
+    paths: triangleCoords,
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  });
+
+  bermudaTriangle.setMap(map);
+}
+
+window.initMap = initMap;
